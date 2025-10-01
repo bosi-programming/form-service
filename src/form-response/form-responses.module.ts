879528abc95db350bfccaa5bfe9e7dfc196ee4cd
Felipe Azevedo Bosi <contato@felipebosi.com>
2025-10-01 10:13:14 -0300

    feat(module): add OwnerModule and Owner entity integration
    Integrates the OwnerModule and Owner entity into the FormResponseModule and FormTemplateModule. This includes adding the Owner schema to MongooseModule configurations and importing the OwnerModule.
    
    This change enables the modules to interact with the Owner entity for extended functionality.

--- src/form-response/form-responses.module.ts
+++ src/form-response/form-responses.module.ts
@@ -6,14 +6,18 @@ import {
   FormResponse,
   FormResponseSchema,
 } from './entities/form-response.entity';
+import { OwnerModule } from 'src/owner/owner.module';
+import { Owner, OwnerSchema } from 'src/owner/entities/owner.entity';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: FormResponse.name, schema: FormResponseSchema },
+      { name: Owner.name, schema: OwnerSchema },
     ]),
+    OwnerModule,
   ],
   controllers: [FormResponseController],
   providers: [FormResponseService],
 })
-export class FormResponseModule {}
+export class FormResponseModule { }
--- src/form-template/form-template.module.ts
+++ src/form-template/form-template.module.ts
@@ -2,11 +2,22 @@ import { Module } from '@nestjs/common';
 import { FormTemplateService } from './form-template.service';
 import { FormTemplateController } from './form-template.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { FormTemplate, FormTemplateSchema } from './entities/form-template.entity';
+import {
+  FormTemplate,
+  FormTemplateSchema,
+} from './entities/form-template.entity';
+import { OwnerModule } from 'src/owner/owner.module';
+import { Owner, OwnerSchema } from 'src/owner/entities/owner.entity';
 
 @Module({
-  imports: [MongooseModule.forFeature([{name: FormTemplate.name, schema: FormTemplateSchema}])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: FormTemplate.name, schema: FormTemplateSchema },
+      { name: Owner.name, schema: OwnerSchema },
+    ]),
+    OwnerModule,
+  ],
   controllers: [FormTemplateController],
   providers: [FormTemplateService],
 })
-export class FormTemplateModule {}
+export class FormTemplateModule { }
